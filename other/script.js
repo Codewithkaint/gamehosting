062228f8b926728c43a358221282dca9e53e5c21
CodeWithKaint <abhishekkaintura071@gmail.com>
2022-04-15 12:28:59 +0530

    web hosting

---
+++ other/script.js
@@ -0,0 +1,65 @@
+let music=new Audio("/other/back.mp3");
+let turn=new Audio("/other/notify.mp3");
+let gameover=new Audio("/other/gameover.mp3");
+let tu="X"
+let isover=false;
+
+
+const change = () =>{
+    return tu==="X"?"0":"X"
+}
+
+const win=()=>{
+let text=document.getElementsByClassName('text');
+let wins=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    
+    [1,4,7],
+    [2,5,8],
+    
+    
+    [0,4,8],
+    [2,4,6],
+
+]
+wins.forEach(e=>{
+    if((text[e[0]].innerText === text[e[1]].innerText) && (text[e[2]].innerText === text[e[1]].innerText) && (text[e[0]].innerText !== "") ){
+        document.querySelector('.info').innerText = text[e[0]].innerText + " Won"
+       isover=true;
+       document.querySelector('.imgs').getElementsByTagName('img')[0].style.width='120px';
+       gameover.play();
+          
+    }
+ 
+})
+}
+music.play();
+let box=document.getElementsByClassName("box");
+Array.from(box).forEach(element=>{
+    let text=element.querySelector('.text');
+    element.addEventListener('click',()=>{
+        if(text.innerText===''){
+            text.innerText=tu;
+            tu=change();
+            turn.play();
+            win();
+            if (isover==false){
+                document.getElementsByClassName("info")[0].innerText  = "TURN - " + tu;
+            } 
+        }
+
+    })
+})
+btn.addEventListener('click',()=>{
+    let text=document.querySelectorAll('.text');
+    Array.from(text).forEach(element=>{
+        element.innerText=""
+    })
+    tu="X";isover=false;
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + tu;
+    document.querySelector('.imgs').getElementsByTagName('img')[0].style.width = "0px"
+    
+})
